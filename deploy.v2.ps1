0ffb5999b25e3e7f6fb55cb2a2ac6c5bf0c93681
Sourabh Rustagi <sourabh.rustagi@hotmail.com>
2023-02-24 01:55:44 +0530

    service monitor change sin selctor for prom

--- deploy.v2.ps1
+++ deploy.v2.ps1
@@ -5,19 +5,19 @@
 
 # helm search repo prometheus-community
 kubectl create ns monitoring
-# helm search repo prometheus-community
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-# helm search repo prometheus-community
-helm install prometheus prometheus-community/kube-prometheus-stack -n monitoring
+
+kubectl apply --server-side -k .\Evolution.infra\deploy\k8s\infra\overlays\dev\
+
+kubectl apply -k .\Evolution.uploader\deploy\k8s\uploader\overlays\dev\
+
+kubectl apply -k .\Evolution.processor\deploy\k8s\processor\overlays\dev\
+
+kubectl apply -k .\Evolution.uploader\deploy\k8s\uploader\overlays\dev\
+
+
 # path for node exporter to work on windows, it deosnt work with docker in widnwosm but works with containerd
 kubectl patch ds prometheus-prometheus-node-exporter --type "json" -p '[{"op": "remove", "path" : "/spec/template/spec/containers/0/volumeMounts/2/mountPropagation"}]' -n monitoring
-        
-
-#^ APPS SETUP
-
-# Mssql secret
-kubectl create secret generic mssql --from-literal=SA_PASSWORD="password@1" -n evolution
-# AWS S3 Access secret
-kubectl create secret generic access --from-literal=AWS_ACCESS_KEY=$env:AWS_ACCESS_KEY -n evolution
-# AWS S3 Access secret
-kubectl create secret generic secret --from-literal=AWS_SECRET_KEY=$env:AWS_SECRET_KEY -n evolution
+     
+# remove a namespace finalizer, requires jq package, ubuntu
+NAMESPACE=uploader; kubectl get namespace $NAMESPACE -o json | jq 'del(.spec.fina
+lizers[0])' | kubectl replace --raw "/api/v1/namespaces/$NAMESPACE/finalize" -f -
