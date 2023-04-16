32487a086928239aa7112aa970be40dd301425e6
Sourabh Rustagi <sourabh.rustagi@hotmail.com>
2023-04-16 23:14:42 +0530

    Add Linkerd Docs

--- deploy.v2.ps1
+++ deploy.v2.ps1
@@ -8,7 +8,29 @@ kubectl create ns monitoring
 
 kubectl apply --server-side -k .\Evolution.infra\deploy\k8s\infra\overlays\dev\
 
-kubectl apply -k .\Evolution.uploader\deploy\k8s\uploader\overlays\dev\
+
+
+#--- 
+
+linkerd install --crds | kubectl apply -f -
+linkerd install --set proxyInit.runAsRoot=true | kubectl apply -f -
+
+export PATH=$PATH:/home/sourabh/.linkerd2/bin
+
+Now run:
+
+  linkerd check --pre                     # validate that Linkerd can be installed
+  linkerd install --crds | kubectl apply -f - # install the Linkerd CRDs
+  linkerd install | kubectl apply -f -    # install the control plane into the 'linkerd' namespace
+  linkerd check                           # validate everything worked!
+
+You can also obtain observability features by installing the viz extension:
+
+  linkerd viz install | kubectl apply -f -  # install the viz extension into the 'linkerd-viz' namespace
+  linkerd viz check                         # validate the extension works!
+  linkerd viz dashboard                     # launch the dashboard
+
+kubectl apply -k .\Evolution.identity\deploy\k8s\identity\overlays\dev\
 
 kubectl apply -k .\Evolution.processor\deploy\k8s\processor\overlays\dev\
 
