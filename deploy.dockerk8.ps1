dd4060f671f85136b6826090b1e55e09f51a728b
Sourabh Rustagi <sourabh.rustagi@hotmail.com>
2023-05-20 19:40:06 +0530

    add docker k8 ps

---
+++ deploy.dockerk8.ps1
@@ -0,0 +1,1 @@
+kumactl install control-plane   --set "cni.enabled=true"   --set "cni.chained=true"   --set "cni.netDir=/etc/cni/net.d"   --set "cni.binDir=/opt/cni/bin"   --set "cni.confName=10-calico.conflist" --set "cni.enabled=true" --set "experimental.cni=true" | --set "experimental.ebpf.enabled=true" | kubectl apply -f .
